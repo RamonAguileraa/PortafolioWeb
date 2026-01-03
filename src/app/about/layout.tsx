0c437bc3e7410a427c3b19e694e93006bec5ff06
RamonAguileraa <ra16monaagui16lera@gmail.com>
2026-01-03 15:52:56 -0600

    refactor: simplify about layout

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import { Inter } from "next/font/google";
-import AboutNavbar from "../components/AboutNavbar";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={`${inter.className} bg-black dark:bg-black light:bg-white text-white dark:text-white light:text-black transition-colors duration-300`}>
-      <AboutNavbar />
-      <div className="pt-24">{children}</div>
-    </div>
-  );
+  return <>{children}</>
 }
